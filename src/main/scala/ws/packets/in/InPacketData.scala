8bf16bee1bc01bc5a03f3f23fc11a21165030488
Amaury Rousseau <kinrar@kinrar.io>
2022-08-11 11:51:38 +0200

    Add API version prefix to data URL in callback

--- src/main/scala/ws/packets/in/InPacketData.scala
+++ src/main/scala/ws/packets/in/InPacketData.scala
@@ -31,7 +31,7 @@ case class InPacketData(request_id: String, last: Boolean) extends WsInPacket {
       _ <- if last then BlazeClientBuilder[IO].resource.use(_.successful(Request[IO](
           Method.POST,
           query.callback,
-        ).withEntity(DataCallback(query.id, true, Some(s"${Env.get.baseUrl}/data/$request_id/$dataPath")))))
+        ).withEntity(DataCallback(query.id, true, Some(s"v1/${Env.get.baseUrl}/data/$request_id/$dataPath")))))
         else IO.println("some other data should follow")
     } yield ()
 }
